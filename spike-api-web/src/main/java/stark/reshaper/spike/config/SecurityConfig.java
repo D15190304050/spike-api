52a97a74a672ddb483dceb386d57c0df66ca9173
D1519304050 <15190304050@163.com>
2023-04-29 09:53:19 +0800

    Security configuration.

--- spike-api-web/src/main/java/stark/reshaper/spike/config/SecurityConfig.java
+++ spike-api-web/src/main/java/stark/reshaper/spike/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package stark.reshaper.spike.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
